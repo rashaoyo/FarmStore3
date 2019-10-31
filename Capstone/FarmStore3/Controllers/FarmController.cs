3f2d414da96f99af147cfd827f01db63d0212e89
Sarah Yoo <rashaoyo@gmail.com>
2019-10-30 21:41:14 -0400

    Adding Framework for Project

--- Capstone/FarmStore3/Controllers/FarmController.cs
+++ Capstone/FarmStore3/Controllers/FarmController.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FarmStore3.Controllers
 {
-    public class FarmController
+    public class FarmController : Controller
     {
+        public IActionResult Produce()
+        {
+            return View();
+        }
+        public IActionResult AddProduce()
+        {
+            return View();
+        }
+
     }
 }
---
+++ Capstone/FarmStore3/FarmStore3Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmStore3
+{
+    public class FarmStore3Configuration
+    {
+    }
+}

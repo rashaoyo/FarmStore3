2ab3a668406dd09f1dee0adc0ff9209ceed51cf3
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-10-31 14:12:48 -0400

    added the add feature

--- Capstone/FarmStore3/FarmServices/IFarmService.cs
+++ Capstone/FarmStore3/FarmServices/IFarmService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FarmStore3.DAL;
+using FarmStore3.DAL.Models;
+using FarmStore3.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +10,59 @@ namespace FarmStore3.FarmServices
 {
     public interface IFarmService
     {
+        ProductViewModel GetProducts();
 
+        ProductViewModel AddNewProduct(Products model);
+    }
+
+    public class ProductService : IFarmService
+    {
+        private readonly IFarmStore _farmStore;
+
+        public ProductService(IFarmStore farmStore)
+        {
+            _farmStore = farmStore;
+        }
+
+        public ProductViewModel GetProducts()
+        {
+            var dalProducts = _farmStore.SelectAllProduct();
+            var products = new List<Products>();
+
+            foreach (var dalProduct in dalProducts)
+            {
+                var product = new Products();
+                product.ProduceName = dalProduct.ProduceName;
+                product.ProduceID = dalProduct.ProduceID;
+                products.Add(product);
+            }
+            var ProductViewModel = new ProductViewModel();
+            ProductViewModel.Products = products;
+
+            return ProductViewModel;
+        }
+
+        public ProductViewModel AddNewProduct(Products model)
+        {
+            var dalModel = new FarmDALModel();
+            dalModel.ProduceName = model.ProduceName;
+            _farmStore.InsertNewProduct(dalModel);
+
+            //MAPPING
+            var dalProducts = _farmStore.SelectAllProduct();
+            var products = new List<Products>();
+
+            foreach (var dalProduct in dalProducts)
+            {
+                var product = new Products();
+                product.ProduceName = dalProduct.ProduceName;
+                products.Add(product);
+            }
+
+            var ProductViewModel = new ProductViewModel();
+            ProductViewModel.Products = products;
+
+            return ProductViewModel;
+        }
     }
 }

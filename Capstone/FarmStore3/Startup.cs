18b0e15a939d9ffa7de3a5ff84309f75ee84d393
nick born <nicholasjborn@gmail.com>
2019-10-30 21:10:24 -0400

    namespace fixed on startup

--- Capstone/FarmStore3/Startup.cs
+++ Capstone/FarmStore3/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace FarmStore
+namespace FarmStore3
 {
     public class Startup
     {
